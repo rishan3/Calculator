7e02db0920375ea5784fc94bd7697715daa70c41
rishan3 <muhammedrishank0@gmail.com>
2023-01-22 10:34:44 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateRate(){
+  var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value; 
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var amount = parseInt(principal) + parseFloat(interest);
+    var result = document.getElementById("result");
+}
