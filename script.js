a56767f1ef66edaf5bda66bd306a3b0a2a9f466c
rishan3 <muhammedrishank0@gmail.com>
2023-01-22 10:55:11 +0530

    calculator

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function updateRate(){
     var amount = parseInt(principal) + parseFloat(interest);
     var result = document.getElementById("result");
 }
+if (principal <= 0) {
+  alert('Please enter a positive number!');
+  document.getElementById("principal").focus();
+}
+else {
+  result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br> in the year " + "<mark>" + year + "</mark>" + "\<br>";
+}
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
